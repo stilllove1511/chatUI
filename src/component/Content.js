21d1f88f8ab3db8117c0a6f0e495b8d04dcd6c15
stilllove11 <100393523+stilllove00@users.noreply.github.com>
2022-02-25 13:48:49 +0700

    css giao dien

--- src/component/Content.js
+++ src/component/Content.js
@@ -1,6 +1,9 @@
 
 import {useState, useEffect, useRef} from 'react'
+import clsx from 'clsx';
+
 import Messgaes from './Messages'
+import ContentStyle from '../componentCSS/Content.module.css'
 
 const channels = [
   {
@@ -61,45 +64,52 @@ function Content(){
     },[channelId])
 
   return (
-      <>
-        <ul>
-          {channels.map((channel) => (
-            <li 
-              key={channel.id} 
-              style={{
-                color:channel.id===channelId?
-                  'red':
-                  '#333'
-                }} 
-              onClick={() => {setId(channel.id)}}
-            >
-              Kênh {channel.id}
-            </li>
-          ))}
-        </ul>
-        <span> Tin nhắn: </span>
-        <br/>
-        <input
-          ref={inputRef}
-          value={inputMessage.content}
-          onChange={e => setInput({isFromMe: true, content:e.target.value })}
-        />
-        <button ref={buttonRef} onClick={handleSubmit}>Add</button>
-        <br/>
-    
-        {messages.map((message, index)=>(
-            <>
-                <Messgaes key={index}>
-                  {message.isFromMe ? 
-                    'Tôi: ':
-                    channels[channelId-1].userName+': '} 
-                  {message.content}
-                </Messgaes>
-                <br/>
-            </>
-        ))}
+      <div className={ContentStyle.content} >
+      
+        {/* channel */}
+        <div className={ContentStyle.channels}>
+          <ul className={ContentStyle.p0}>
+            {channels.map((channel) => (
+              <li 
+                className = {clsx(ContentStyle.card, {
+                  [ContentStyle.active] : channel.id===channelId
+                })}
+                key={channel.id} 
+                onClick={() => {setId(channel.id)}}
+              >
+                Kênh {channel.id}
+              </li>
+            ))}
+          </ul>
+        </div>
         
-      </>
+        {/* detail */}
+        <div className={ContentStyle.detail}>
+          <span> Tin nhắn: </span>
+          <br/>
+          {/* input */}
+          <input
+            ref={inputRef}
+            value={inputMessage.content}
+            onChange={e => setInput({isFromMe: true, content:e.target.value })}
+          />
+          <button ref={buttonRef} onClick={handleSubmit}>Add</button>
+          <br/>
+        {/* messages */}
+          {messages.map((message, index)=>(
+              <>
+                  <Messgaes key={index}>
+                    {message.isFromMe ? 
+                      'Tôi: ':
+                      channels[channelId-1].userName+': '} 
+                    {message.content}
+                  </Messgaes>
+                  <br/>
+              </>
+          ))}
+        </div>
+        
+      </div>
   );
 }
       
